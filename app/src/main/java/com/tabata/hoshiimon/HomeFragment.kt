d5a8c635956944bd49a2f72806d76c2279283f1d
tabata <tabata0445@gmail.com>
2022-11-25 23:16:00 +0900

    [add]Data Bindingの追加

--- app/src/main/java/com/tabata/hoshiimon/HomeFragment.kt
+++ app/src/main/java/com/tabata/hoshiimon/HomeFragment.kt
@@ -5,12 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.tabata.hoshiimon.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        val binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
